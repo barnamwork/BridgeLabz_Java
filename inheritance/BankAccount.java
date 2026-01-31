3d063ec35ab27e774ed6077f5233d5391040b171
Barnamwork <barnamwork@gmail.com>
2026-01-31 23:39:56 +0530

    [Barnam]: Started Hierarchical inheritance

---
+++ inheritance/BankAccount.java
@@ -0,0 +1,72 @@
+package inheritance;
+
+public class BankAccount {
+    int accNo;
+
+    BankAccount(int accNo){
+        this.accNo = accNo;
+    }
+
+    void displayAccountType(){
+        System.out.println("Account no: " + accNo);
+        System.out.println();
+    }
+
+    public static void main() {
+        BankAccount bankaccount = new BankAccount(101);
+        bankaccount.displayAccountType();
+        System.out.println();
+
+        SavingsAccount savingaccount = new SavingsAccount(102, 8.5);
+        savingaccount.displayAccountType();
+        System.out.println();
+
+        CheckingAmount checkingamount = new CheckingAmount(103, 120000.0);
+        checkingamount.displayAccountType();
+        System.out.println();
+
+        FixedDepositAccount fixed = new FixedDepositAccount(104, 7.5);
+        fixed.displayAccountType();
+        System.out.println();
+    }
+
+}
+class SavingsAccount extends BankAccount{
+    double interestRate;
+
+    SavingsAccount(int accNo, double interestRate){
+        super(accNo);
+        this.interestRate = interestRate;
+    }
+
+    void displayAccountType(){
+        System.out.println("Account no: " + accNo);
+        System.out.println("Interest Rate: " + interestRate);
+    }
+}
+class CheckingAmount extends BankAccount{
+    double withdrawalLimit;
+
+    CheckingAmount(int accNo, double withdrawalLimit){
+        super(accNo);
+        this.withdrawalLimit=withdrawalLimit;
+    }
+
+    void displayAccountType(){
+        System.out.println("Account no: " + accNo);
+        System.out.println("Withdrawal Limit: " + withdrawalLimit);
+    }
+}
+class FixedDepositAccount extends BankAccount{
+
+    double fixedRate;
+    FixedDepositAccount(int accNo, double fixedRate){
+        super(accNo);
+        this.fixedRate=fixedRate;
+    }
+    void displayAccountType(){
+        System.out.println("Account no: " + accNo);
+        System.out.println("Fixed Rate: " + fixedRate);
+    }
+}
+
--- inheritance/Course.java
+++ inheritance/Course.java
@@ -1,6 +1,60 @@
 package inheritance;
 
 public class Course {
+    String courseName;
+    double duration;
+    Course(String courseName, double duration){
+        this.courseName=courseName;
+        this.duration=duration;
+    }
+    void displayDetails(){
+        System.out.println("Coursename : " + courseName);
+        System.out.println("Duration : " + duration);
+    }
+
+    public static void main() {
+        Course course = new Course("calisthenics", 20);
+        OnlineCourse onlinecourse = new OnlineCourse("Online Course", 40, "PhysicsWallah", true);
+        PaidOnlineCourse paidonlinecourse = new PaidOnlineCourse("Paid Course", 60, "BridgeLabz", true, 45000, 10 );
+
+        course.displayDetails();
+        System.out.println();
+        onlinecourse.displayDetails();
+        System.out.println();
+        paidonlinecourse.displayDetails();
+    }
+
+}
+class OnlineCourse extends Course {
+    String platform;
+    Boolean isRecorded;
+    OnlineCourse(String courseName, double duration, String platform, Boolean isRecorded){
+        super(courseName, duration);
+        this.platform=platform;
+        this.isRecorded=isRecorded;
+    }
+
+    @Override
+    void displayDetails(){
+        super.displayDetails();
+        System.out.println("Platform: " + platform);
+        System.out.println("isRecorded: " + isRecorded);
+    }
+}
+class PaidOnlineCourse extends OnlineCourse{
+    double fee;
+    double discount;
+    PaidOnlineCourse(String courseName, double duration, String platform, Boolean isRecorded, double fee, double discount){
+        super(courseName, duration, platform, isRecorded);
+        this.fee=fee;
+        this.discount=discount;
+    }
+    @Override
+    void displayDetails(){
+        super.displayDetails();
+        System.out.println("Fee: " + fee);
+        System.out.println("Discount: " + discount);
+    }
 
 }
 
