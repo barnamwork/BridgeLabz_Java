5b8dd8aebe361bcd47c08e6fdde3b06fdd74cd71
Barnamwork <barnamwork@gmail.com>
2026-02-04 23:50:45 +0530

    [Barnam] : minor code change

--- linkedList/StudentManagement.java
+++ linkedList/StudentManagement.java
@@ -28,10 +28,12 @@ class StudentSinglyLinkedList{
 
     public void addAtEnd(int rollNumber, String name, int age, String grade){
         StudentNode newNode = new StudentNode(rollNumber, name, age, grade);
+
         if(head == null){
-            head=newNode;
+            head = newNode;
             return;
         }
+
         StudentNode temp = head;
 
         while(temp!=null){
@@ -41,7 +43,7 @@ class StudentSinglyLinkedList{
     }
 
 
-    public void deleteById(int rollNo){
+    public void deleteById (int rollNo){
         if (head == null){
             System.out.println("The list is empty");
             return;
@@ -55,15 +57,71 @@ class StudentSinglyLinkedList{
         StudentNode prev = head;
         StudentNode curr = head.next;
 
-        while (curr!=null && curr.rollNumber!=rollNo){
-
+        while (curr != null && curr.rollNumber != rollNo){
+            prev = curr;
+            curr = curr.next;
+        }
+        if(curr == null){
+            System.out.println("Student not found");
+        }
+        else {
+            prev.next = curr.next;
+            System.out.println("Student deleted: " + rollNo + " Successfully");
         }
 
     }
-    public void searchEmployee(){
+    public void searchStudent(int rollNo){
+        StudentNode temp = head;
+
+        while( temp!=null ){
+            if( temp.rollNumber == rollNo ){
+                System.out.println("Student found");
+                System.out.println("Name: " + temp.name + ", Age: " + temp.age +m ", Grade: " + temp.grade );
+                return;
+            }
+            temp=temp.next;
+        }
+        System.out.println("Student not found");
 
     }
     public void display(){
+        if (head == null) {
+            System.out.println("No records available");
+            return;
+        }
+
+        StudentNode temp =head;
+        while(temp!= null){
+            System.out.println( temp.name+ " | " + temp.age + " | " + temp.grade + " | " + temp.rollNumber);
+            temp=temp.next;
+        }
+    }
+
+    public void insertAtPosition(int rollNumber, String name, int age, String grade, int position){
+        if(position<=0){
+            System.out.println("Position cannot be 0");
+            return;
+        }
+        if(position ==1){
+            addAtBeginning(rollNumber, name, age, grade);
+            return;
+        }
+
+        StudentNode newNode = new StudentNode( rollNumber, name, age, grade );
+        StudentNode temp = head;
+        for( int i=1; i < position-1 && temp!=null; i++){
+            temp = temp.next;
+        }
+        if(temp == null){
+            System.out.println(" out of range : position ");
+            return;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public void deleteFromBeginning(){
 
     }
 
